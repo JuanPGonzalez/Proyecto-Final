e7d0eb653d11937389e2833225dd021424ed5f8a
JuanPGonzalez <99921443+JuanPGonzalez@users.noreply.github.com>
2026-06-15 23:22:06 -0300

    correccion

--- backend/services/pricingCron.js
+++ backend/services/pricingCron.js
@@ -2,8 +2,8 @@ const cron = require('node-cron');
 const { updatePrices } = require('./pricingEngine');
 
 const startPricingCron = () => {
-  // Ejecutar una vez al día a las 03:00 AM
-  cron.schedule('0 3 * * *', async () => {
+  // Ejecutar una vez al día a las 00:00 AM (Medianoche)
+  cron.schedule('0 0 * * *', async () => {
     await updatePrices();
   }, {
     scheduled: true,
